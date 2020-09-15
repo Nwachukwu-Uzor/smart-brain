2970a87b906810878728b89e2260128338d3fed0
uzor1997 <uzor1997@gmail.com>
2020-09-15 22:42:14 +0100

    changed the start command in package.jsn

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -11,7 +11,7 @@ class Signin extends React.Component {
 
     onEmailChange = (event) => {
       this.setState({signInEmail: event.target.value})
-    } 
+    }
 
     onPasswordChange = (event) => {
       this.setState({signInPassword: event.target.value})
@@ -45,30 +45,30 @@ class Signin extends React.Component {
                   <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                   <div className="mt3">
                       <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                      <input 
-                          className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                          type="email" 
-                          name="email-address"  
-                          id="email-address" 
+                      <input
+                          className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                          type="email"
+                          name="email-address"
+                          id="email-address"
                           onChange={this.onEmailChange}
                       />
                   </div>
                   <div className="mv3">
                       <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                      <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                          type="password" 
-                          name="password" 
+                      <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                          type="password"
+                          name="password"
                           id="password"
-                          onChange={this.onPasswordChange} 
+                          onChange={this.onPasswordChange}
                       />
                   </div>
               </fieldset>
           <div className="">
-              <input 
+              <input
                   onClick={this.onSubmitSignin}
-                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                  type="button" 
-                  value="Sign in" 
+                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                  type="button"
+                  value="Sign in"
               />
           </div>
           <div className="lh-copy mt3">
